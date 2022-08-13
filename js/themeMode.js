41cb98681c115142326622ec3ad4b60ebf8c850b
Lucas Ginard <contactolucasginard@gmail.com>
2022-08-12 21:04:29 -0400

    add icon apple , add playstore profile

--- js/themeMode.js
+++ js/themeMode.js
@@ -3,9 +3,12 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        document.getElementById("iconApple").src="images/icon_white_apple.png"
+
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        document.getElementById("iconApple").src="images/icon_black_apple.png"
     }
 }
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
@@ -14,6 +17,7 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+        document.getElementById("iconApple").src="images/icon_white_apple.png"
     }
 }
 toggleSwitch.addEventListener('change', switchTheme, false);
